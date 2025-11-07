e15e0370beabf5e5871046416e076dca5c4a83c3
Arfat Chowdhury <arfatahsan60@gmail.com>
2025-11-07 19:19:30 +0600

    setup nativwind

---
+++ babel.config.js
@@ -0,0 +1,30 @@
+module.exports = function (api) {
+    api.cache(true);
+    return {
+      presets: [
+        ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+        "nativewind/babel",
+      ],
+    };
+  };
+
+
+// module.exports = {
+//     presets: ['module:metro-react-native-babel-preset'],
+//     plugins: [
+//       // ... other plugins
+//       'react-native-worklets/plugin', // Make sure this line exists and is correct
+//       // If you use Reanimated, its plugin should be the last one:
+//       // 'react-native-reanimated/plugin', 
+//     ],
+//   };
+  
+      // babel.config.js
+      // module.exports = function(api) {
+      //   api.cache(true);
+      //   return {
+      //     presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset'], // Ensure this line exists and is correct
+      //   };
+      // };
+  
+  
