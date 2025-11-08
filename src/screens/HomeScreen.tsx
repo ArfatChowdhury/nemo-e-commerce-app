3c84673af97f1f209cc037d3f790d6f1d94d2579
Arfat Chowdhury <arfatahsan60@gmail.com>
2025-11-08 17:17:42 +0600

    setup nativwind

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,19 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextInput } from 'react-native'
 import React from 'react'
+
+import { Ionicons } from '@expo/vector-icons'
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View className=' mt-2 flex-1'>
+      <View className='flex flex-row items-center bg-white border border-gray-300 rounded-full px-4 py-3 mx-4  shadow-sm'>
+      <Ionicons name='search' size={24}  color='#6B7280'/>
+        <TextInput
+          placeholder='Search desire product'
+          className='flex-1 text-base'
+        />
+      </View>
+
     </View>
   )
 }
