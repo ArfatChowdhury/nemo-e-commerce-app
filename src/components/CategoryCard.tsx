8237a4de415bcf94d8779c20ad97de816e3e469c
Arfat Chowdhury <arfatahsan60@gmail.com>
2025-11-23 18:52:07 +0600

    adding typeOf define in every components

--- src/components/CategoryCard.tsx
+++ src/components/CategoryCard.tsx
@@ -1,10 +1,17 @@
 import { View, Text, Pressable } from 'react-native'
 import React from 'react'
 
-const CategoryCard = ({ item, handleCategory }) => {
+
+
+interface CategoryCardProps {
+    item: string,
+    handleCategory: (category: string) => void
+}
+
+const CategoryCard = ({ item, handleCategory }: CategoryCardProps) => {
     return (
-        <Pressable 
-            onPress={() => handleCategory(item)} 
+        <Pressable
+            onPress={() => handleCategory(item)}
             className='flex-1 items-center justify-center p-6 m-2 bg-white border border-gray-200 rounded-xl shadow-sm mb-10'
         >
             <Text className='text-center text-base font-medium text-gray-800'>{item}</Text>
