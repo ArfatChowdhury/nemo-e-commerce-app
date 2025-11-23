216694f67d1a9a309f750681762d7f7befc990af
Arfat Chowdhury <arfatahsan60@gmail.com>
2025-11-23 19:32:05 +0600

    adding typeOf define in every components

--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -1,20 +1,27 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import React, { ComponentProps } from 'react'
 import { Ionicons } from '@expo/vector-icons'
 import { useNavigation } from '@react-navigation/native'
 
-const HeaderBar = ({title, iconName, size=24}) => {
+
+interface HeaderBarProps {
+    title: string;
+    iconName: ComponentProps<typeof Ionicons>['name'];
+    size?: number;
+}
+
+const HeaderBar = ({ title, iconName, size = 24 }: HeaderBarProps) => {
     const navigation = useNavigation()
     return (
         <View className="  py-4  flex-row items-center">
             {/* Back Button */}
-            <TouchableOpacity 
+            <TouchableOpacity
                 onPress={() => navigation.goBack()}
                 className="mr-3"
             >
                 <Ionicons name={iconName} size={size} color='black' />
             </TouchableOpacity>
-            
+
             {/* Page Title */}
             <Text className="text-xl font-bold flex-1 text-center">{title}</Text>
         </View>
--- src/components/ImagePicker.tsx
+++ src/components/ImagePicker.tsx
@@ -1,39 +1,38 @@
 import { View, Text, Button } from 'react-native'
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import{addImage} from '../Store/slices/productFormSlice'
+import { useAppDispatch, useAppSelector } from '../Store/hooks'
+import { addImage } from '../Store/slices/productFormSlice'
 import * as ImagePicker from 'expo-image-picker'
 
 const NewImagePicker = () => {
-    const dispatch = useDispatch()
-    const images = useSelector(state => state.productForm.images)
+  const dispatch = useAppDispatch()
+  const images = useAppSelector(state => state.productForm.images)
 
-    const imagePick = async () => {
-        let result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ['images'],
-            allowsEditing: true,
-            aspect: [4,3],
-            quality: 1,
-            allowsMultipleSelection:true,
-            selectionLimit: 5
-        })
+  const imagePick = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+      allowsMultipleSelection: true,
+      selectionLimit: 5
+    })
 
-        if(!result.canceled){
-            const newImageUris = result.assets.map(asset => asset.uri)
+    if (!result.canceled) {
+      const newImageUris = result.assets.map(asset => asset.uri)
 
-            newImageUris.forEach(uri => {
-                 dispatch(addImage(uri))
-           })
-        }
-        
+      newImageUris.forEach(uri => {
+        dispatch(addImage(uri))
+      })
     }
+
+  }
 
   return (
     <View>
-      <Button onPress={imagePick} title='open gallery'/> 
+      <Button onPress={imagePick} title='open gallery' />
     </View>
   )
 }
 
 export default NewImagePicker
-
